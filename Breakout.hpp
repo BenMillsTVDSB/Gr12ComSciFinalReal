87ae39894851c06f87910b8e686cb24a3f09a035
Owen Gurr <339620759@gotvdsb.ca>
2024-01-09 14:09:07 -0500

    This is a test

--- Breakout.hpp
+++ Breakout.hpp
@@ -1,7 +1,7 @@
 #include "GeneralHeader.hpp"
 
 bool mainBreakout()// returns true if player clears section, false if they are defeated. Program ends upon returning false.
-{
+{// test
     // Gameplay loop
     while(!WindowShouldClose())
     {
