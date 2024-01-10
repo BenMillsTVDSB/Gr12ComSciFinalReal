e0321f2568c8660ed2571e3b33e346acdfaaa4c5
Ben Mills <339480535@gotvdsb.ca>
2024-01-10 14:28:08 -0500

    added player class

--- GeneralHeader.hpp
+++ GeneralHeader.hpp
@@ -6,11 +6,89 @@ enum gameMode : char {slidingPuzzle, breakout, platformer};// Potentially unnece
 
 enum direction : char {left, right, up, down};
 
+class PlatformerPlayer
+{
+    private:
+        Rectangle hitbox;
+        Vector2 velocity;
+        Color colour;
+        bool grounded;
+        float xAcceleration;
+        float airXAcceleration;
+
+    public:
+        PlatformerPlayer(Rectangle inHitbox, Vector2 inVelocity, Color inColour, float inAirXAcceleration)
+        {
+            hitbox = inHitbox;
+            velocity = inVelocity;
+            colour = inColour;
+            grounded = false;
+            airXAcceleration = inAirXAcceleration;
+            xAcceleration = inAirXAcceleration;
+        }
+
+        Rectangle getHitbox()
+        {
+            return hitbox;
+        }
+
+        void setYPosition(float inYPosition)
+        {
+            hitbox.y = inYPosition;
+        }
+
+        void setXPosition(float inXPosition)
+        {
+            hitbox.x = inXPosition;
+        }
+
+        float getHeight()
+        {
+            return hitbox.height;
+        }
+
+        float getWidth()
+        {
+            return hitbox.width;
+        }
+
+        Vector2 getVelocity()
+        {
+            return velocity;
+        }
+
+        void setYVelocity(float inYVelocity)
+        {
+            velocity.y = inYVelocity;
+        }
+
+        void setXVelocity(float inXVelocity)
+        {
+            velocity.x = inXVelocity;
+        }
+
+        void setGrounded(bool inGrounded)
+        {
+            grounded = inGrounded;
+        }
+
+        void setXAcceleration(float inXAcceleration)
+        {
+            xAcceleration = inXAcceleration;
+        }
+
+        void update()
+        {
+
+        }
+};
+
 class Brick
 {
     private:
         Rectangle hitbox; //= {20, 20, 500, 500};// x, y, width, height
         Color colour;
+        float friction;
 
         direction rectangleEnteredFromSide(Rectangle otherHitbox, Vector2 otherVelocity)// Used if other was outside hitbox on the previous frame. up = top side, down = bottom side. Only works if you already know that other is touching hitbox already. otherVelocity must be in pixels per second, ensure that otherVelocity is the velocity other used for their last movement.
         {
@@ -35,19 +113,50 @@ class Brick
                 return down;
             }
 
-            otherHitbox.x = 0/0;// If the game crashed and you ended up here, then there was a problem with either this code or how it was used. Let the group know.
+            otherHitbox.x = 0/0;// If the game crashed and you ended up here, then there was a problem with either this method or how it was called. Tell group.
         }
 
     public:
-        Brick(Rectangle inHitbox, Color inColour)
+        Brick(Rectangle inHitbox, Color inColour, float inFriction)
         {
             hitbox = inHitbox;
             colour = inColour;
+            friction = inFriction;
         }
 
-        virtual void update() {}// Can be used if you need a subclass.
+        virtual void update() {}// Can be used in subclases, will be called every frame.
 
-        
+        virtual void checkAndHandleColisionPlatformer(PlatformerPlayer player)
+        {
+            direction collisionSide;
+            
+            if(!CheckCollisionRecs(hitbox, player.getHitbox())) return;
+
+            collisionSide = rectangleEnteredFromSide(player.getHitbox(), player.getVelocity());
+
+            switch(collisionSide)
+            {
+                case up:
+                    player.setGrounded(true);
+                    player.setYVelocity(0);
+                    player.setYPosition(hitbox.y - player.getHeight());
+                    player.setXAcceleration(friction);
+                    break;
+                case left:
+                    player.setXVelocity(0);
+                    player.setXPosition(hitbox.x - player.getWidth());
+                    break;
+                case right:
+                    player.setXVelocity(0);
+                    player.setXPosition(hitbox.x + hitbox.width);
+                    break;
+                case down:
+                    player.setYVelocity(0);
+                    player.setYPosition(hitbox.y + hitbox.height);
+                    break;
+
+            }
+        }
 };
 
 void gameOverScreen()
