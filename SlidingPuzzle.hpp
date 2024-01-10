9000fdf54a01f098b74a58e97ee3247d9baa6a98
Amulya <khanalamulya2@gmail.com>
2024-01-10 13:50:23 -0500

    //New changes

--- SlidingPuzzle.hpp
+++ SlidingPuzzle.hpp
@@ -1,4 +1,5 @@
 #include "GeneralHeader.hpp"
+#include "raylib.h"
 
 bool mainSlidingPuzzle()// returns true if player clears section, false if they are defeated. Program ends upon returning false.
 {
@@ -6,9 +7,19 @@ bool mainSlidingPuzzle()// returns true if player clears section, false if they 
     while(!WindowShouldClose())
     {
         BeginDrawing();
+        InitWindow(400, 300, "SlidingPuzzle");
+        SetTargetFPS(30);
+
+        ClearBackground(RAYWHITE);
+        DrawCircle(400, 0, 10, RED);
+        DrawRectangle(400, 100, 50, 60, BLACK);
+
+        //if(IsKeyPressed(KEY_LEFT))
+
+
+
         EndDrawing();
 
-        // hello
     }
     
     return false;
