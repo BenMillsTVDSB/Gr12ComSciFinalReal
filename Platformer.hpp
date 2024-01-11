3d8d7997fa10fcf7171eafcad0217ff9a1f7bd18
Ben Mills <339480535@gotvdsb.ca>
2024-01-11 13:58:42 -0500

    fixed crash

--- Platformer.hpp
+++ Platformer.hpp
@@ -3,7 +3,7 @@
 void mainPlatformer()
 {
     PlatformerPlayer player = PlatformerPlayer({10, 10, 40, 40}, WHITE, 400, 300, 99900, 100);
-    Brick brick = Brick({20, 400, 200, 50}, BLUE, 700);
+    Brick brick = Brick({20, 400, 200, 50}, BLUE, 700, 500);
 
     // Gameplay loop
     while(!WindowShouldClose())
