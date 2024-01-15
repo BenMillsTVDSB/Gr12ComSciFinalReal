17625e57a04624e1e7356d46afbd24591bea5ed5
Ben Mills <339480535@gotvdsb.ca>
2024-01-15 13:51:48 -0500

    started to remove subclass support for brick

--- Platformer.hpp
+++ Platformer.hpp
@@ -1,9 +1,9 @@
 #include "GeneralHeader.hpp"
 
-void mainPlatformer(vector<Brick*> & bricks2)
+void mainPlatformer(vector<Brick> & bricks2)
 {
     PlatformerPlayer player = PlatformerPlayer({10, 10, 40, 40}, WHITE, 700, 800, 300, 500, 0.2);
-    vector<Brick*> bricks = {new Brick({20, 400, 200, 50}, BLUE, 2000, 700), new Brick({250, 300, 200, 100}, BLUE, 2000, 700), new Brick({250, 50, 200, 100}, BLUE, 2000, 700), new Brick({460, 300, 200, 100}, BLUE, 2000, 700)};
+    vector<Brick> bricks = {Brick({20, 400, 200, 50}, BLUE, 2000, 700), Brick({250, 300, 200, 100}, BLUE, 2000, 700), Brick({250, 50, 200, 100}, BLUE, 2000, 700), Brick({460, 300, 200, 100}, BLUE, 2000, 700)};
     bool playerOnBrickTop;
 
     // Gameplay loop
@@ -14,7 +14,7 @@ void mainPlatformer(vector<Brick*> & bricks2)
         playerOnBrickTop = false;
         for(int i = 0; i < bricks.size(); i++)
         {
-            if(bricks[i]->updatePlatformer(player))
+            if(bricks[i].updatePlatformer(player))
             {
                 playerOnBrickTop = true;
             }
@@ -29,7 +29,7 @@ void mainPlatformer(vector<Brick*> & bricks2)
         
         for(int i = 0; i < bricks.size(); i++)
         {
-            bricks[i]->draw();
+            bricks[i].draw();
         }
 
         EndDrawing();
